9e7b3de84d5b7b9f1cda64ae341d576fd22da370
charlourdes <charlotterr@hotmail.co.uk>
2025-10-14 22:01:48 +0100

    Refactor project structure: remove unused project card functionality and clean up styles for improved layout

--- project.js
+++ project.js
@@ -1,36 +1,4 @@
-let projectData = [
-    {
-        image: 'img/project-1.png',
-        name: 'project one',
-        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
-        github: '#',
-        live: '#',
-        tags: '#javascript, #fullstack, #css'
-    },
-]
 
-const createProjectCards = (data) => {
-    let projectContainer = document.querySelector('.project-container');
-
-    projectContainer.innerHTML += `
-            <div class="project-card" data-tags="${data.tags}">
-                <div class="project-wrapper">
-                    <div class="project-thumbnail">
-                        <img src="img/close.png" class="close-btn" alt="">
-                        <img src="${data.image}" class="project-img" alt="">
-                        <span class="tags">${data.tags}</span>
-                    </div>
-
-                    <div class="project-body">
-                        <h1 class="project-name">${data.name}</h1>
-                        <p class="project-detail">${data.detail}</p>
-                        <a href="${data.github}" class="btn">github</a>
-                        <a href="${data.live}" class="btn">see live</a>
-                    </div>
-                </div>
-            </div>
- `;
-}
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -189,3 +157,16 @@ document.addEventListener('DOMContentLoaded', updateUKTime);
 
 // Update every second
 setInterval(updateUKTime, 1000);
+
+
+
+// nav toggle 
+
+let links = document.querySelectorAll('.links');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(item => item.classList.remove('active'))
+        link.classList.add('active');
+    })
+})
